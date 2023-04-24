d8aa2d4bb998d0626cdaea9ac0c54b63730addf7
dimitrijeostojic <dimitrije219@gmail.com>
2023-04-25 01:47:55 +0200

    first commit

---
+++ src/components/Expenses/ExpensesChart.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Chart from '../Chart/Chart'
+
+const ExpensesChart = (props) => {
+    const chartDataPoints = [
+        {
+            label: 'Jan',
+            value: 0,
+        },
+        {
+            label: 'Feb',
+            value: 1,
+        },
+        {
+            label: 'Mar',
+            value: 2,
+        },
+        {
+            label: 'Apr',
+            value: 3,
+        },
+        {
+            label: 'May',
+            value: 4,
+        },
+        {
+            label: 'Jun',
+            value: 5,
+        },
+        {
+            label: 'Jul',
+            value: 6,
+        },
+        {
+            label: 'Aug',
+            value: 7,
+        },
+        {
+            label: 'Sep',
+            value: 8,
+        },
+        {
+            label: 'Oct',
+            value: 9,
+        },
+        {
+            label: 'Nov',
+            value: 10,
+        },
+        {
+            label: 'Dec',
+            value: 11,
+        }
+    ]
+
+    for (const expense of props.expenses) {
+        const expenseMonth = expense.date.getMonth();
+        chartDataPoints[expenseMonth].value += expense.amount;
+    }
+
+    return (
+        <Chart dataPoints={chartDataPoints} />
+    )
+}
+
+export default ExpensesChart
---
+++ src/components/NewExpenses/NewExpense.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import ExpenseForm from './ExpenseForm'
+import './NewExpense.css'
+
+
+const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const SaveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+        setIsEditing(false);
+    }
+
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandeler = () => {
+        setIsEditing(false);
+    }
+
+    return (
+        <div className='new-expense'>
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} onCancel={stopEditingHandeler} />}
+        </div>
+    )
+}
+
+export default NewExpense
